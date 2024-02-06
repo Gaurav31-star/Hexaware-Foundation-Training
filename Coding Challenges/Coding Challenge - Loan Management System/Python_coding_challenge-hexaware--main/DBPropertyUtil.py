8783f687e5262cfc261fbfb26f6436ffc499144b
Gaurav Sehgal <61489709+Gaurav31-star@users.noreply.github.com>
2024-02-06 12:53:13 +0530

    Delete Coding Challenges/Coding Challenge - Loan Management System/Python_coding_challenge-hexaware--main directory

--- Coding Challenges/Coding Challenge - Loan Management System/Python_coding_challenge-hexaware--main/DBPropertyUtil.py
+++
@@ -1,7 +0,0 @@
-class PropertyUtil:
-    def getPropertyString():
-        host = 'localhost'
-        username = 'root'
-        password = 'Root'
-        database = 'loan_management_system'
-        return host, username, password, database
--- Coding Challenges/Coding Challenge - Loan Management System/Python_coding_challenge-hexaware--main/database.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector as con
-m1=con.connect(host="localhost",user="root",password="Root",port="3306")
-cur=m1.cursor()
-cur.execute("create database loan_management_system")
